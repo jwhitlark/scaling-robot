91d604d25e2ae58f8c17ae1d6a755c8ce40f0048
Jason Whitlark <jason@whitlark.org>
2015-01-20 15:26:58 -0800

    initial commit

---
+++ test/scaling_robot/core_test.clj
@@ -0,0 +1,7 @@
+(ns scaling-robot.core-test
+  (:require [clojure.test :refer :all]
+            [scaling-robot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
